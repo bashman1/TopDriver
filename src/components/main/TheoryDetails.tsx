fc06dbf50f461ea87e0d645216a83f74fdb9ae90
wamula bashir <wamulabash1@gmail.com>
2024-05-01 19:41:47 +0300

    worked on the theories

--- src/components/main/TheoryDetails.tsx
+++ src/components/main/TheoryDetails.tsx
@@ -5,48 +5,55 @@ import { Dimensions } from "react-native";
 import {appColor} from '../../services/CommonService'
 import IonIcon from 'react-native-vector-icons/Ionicons';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {filter} from '../../services/CommonService';
-import {menu, iconImg} from '../../intro-data';
+import {filter, findObj} from '../../services/CommonService';
+import {menu, iconImg,  theories} from '../../intro-data';
 const screenWidth = Dimensions.get("window").width;
+import { useFocusEffect } from '@react-navigation/native';
 
 const TheoryDetails = (props: any) => {
 
-    const [selectedItems, setSelectedItems] = useState([])
+    const [selectedItems, setSelectedItems] = useState({})
 
-    const [items, setItems] = useState([])
 
     const goToDetails=(element)=>{
         props.navigation.navigate("IconDetailsPage", { id: element.id })
     }
 
     const filterItems = () => {
-        setSelectedItems(filter(iconImg, props.route.params.ind));
+
+        setSelectedItems(findObj(theories, props.route.params.ind));
     }
 
     useEffect(() => {
-        filterItems();
+        console.log(props.route.params.ind)
+  
+       
     }, []);
+
+    useFocusEffect(
+        React.useCallback(() => {
+            filterItems();
+            return () => {
+            };
+        }, [])
+    );
 
     return (
         <ScrollView>
             <View style={styles.padding}>
-                <View style={styles.marginBottom}>
-                    <Text>Testing the theory details page</Text>
-                    {/* <View style={{flexDirection:'row',flex:1,alignItems:'center', flexWrap:'wrap', justifyContent:'center', columnGap:10, rowGap:10}}>
-                        {
-                            selectedItems.map((element:any, i) => 
-                                <TouchableOpacity style={[styles.cardNew,styles.gridItemNew]} onPress={() => {goToDetails(element)}}>
-                                    <View style={{justifyContent:'center',alignItems:'center', width:"100%", height:160, columnGap:10}}>
-                                        
-                                            <Image style={styles.homeScreenIconNew} key={i} source={element.img} />
-                                            <Text style={{textAlign:'center', paddingBottom:10,fontSize:15,width:'100%', color:"black", lineHeight:23, maxWidth:"80%"}}>{element.name}</Text>
-                                       
-                                    </View>
-                                    </TouchableOpacity>
-                                
+                <View style={[styles.marginBottom, styles.card]}>
+                    {
+                       selectedItems.theory && selectedItems.theory.map((obj)=>{
+                            return (
+                                <View>
+                                {obj.element=="header" && <Text style={[styles.healthPalBlue, styles.fontWeightBold, styles.fontSize20, styles.marginBottom]} key={obj.content}>{obj.content}</Text>}
+                                {obj.element=="image" &&  <Image style={styles.homeScreenIconNew} key={obj.content} source={obj.content} />}
+                                {obj.element=="paragraph" && <Text style={[ styles.fontSize15, styles.marginBottom]} key={obj.content}>{obj.content}</Text>}
+                                {obj.element=="bullets" && obj.content.map((data)=> { return (<Text style={styles.fontSize15} key={data}><IonIcon name="ellipse-outline" color={'#000'} size={12}/>{data}</Text>)})}
+                                </View>
                             )
-                        }
-                    </View> */}
+                        })
+                    }
                 </View>
             </View>
         </ScrollView>
